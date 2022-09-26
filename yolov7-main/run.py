4eb99ad913b7a96f1cb0e193605dbec1c79e4a82
kangkc09 <kangkc09@pusan.ac.kr>
2022-09-26 22:04:04 +0900

    chore: change path

--- yolov7-main/run.py
+++ yolov7-main/run.py
@@ -1,5 +1,5 @@
 import os
 
-terminal_command = f"python yolov7-main/modified_detect.py --weights yolov7-main/runs/train/exp/weights/best.pt --conf 0.1 --source image/test.jpg"
+terminal_command = f"python yolov7-main/modified_detect.py --weights runs/train/exp/weights/best.pt --conf 0.1 --source image/test.jpg"
 
 os.system(terminal_command)
